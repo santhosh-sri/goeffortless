e3bdb73790c15d009aeba1c7057bd780dd43be3b
santhosh-sri <santhosh.k@goeffortless.ai>
2026-01-09 18:17:19 +0530

    ui update

--- src/components/FeaturesComponent/MetricCards.tsx
+++ src/components/FeaturesComponent/MetricCards.tsx
@@ -7,16 +7,16 @@ interface Props {
 
 const MetricCards: React.FC<Props> = ({ cards }) => {
   return (
-    <section className="md:w-[1036px] mx-auto">
+    <section className="w-full md:w-[1036px] md:mx-auto max-md:px-5">
       <div className="">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-8">
           {cards.map((card, idx) => (
             <div
               key={idx}
-              className="border border-white/10 bg-[linear-gradient(111.18deg,rgba(255,255,255,0.1)_-28.62%,rgba(255,255,255,0)_104.36%)] rounded-lg p-5 text-center flex flex-col gap-4"
+              className="border border-white/10 bg-[linear-gradient(111.18deg,rgba(255,255,255,0.1)_-28.62%,rgba(255,255,255,0)_104.36%)] rounded-lg p-4 md:p-5 text-center flex flex-col gap-3 md:gap-4"
             >
               {/* Value */}
-              <div className="text-[32px] md:text-[40px] font-medium text-[#F08B32]">
+              <div className="text-[28px] md:text-[40px] font-medium text-[#F08B32]">
                 {card.value}
               </div>
 
