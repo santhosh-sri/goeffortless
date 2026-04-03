c299c9b3bfca0ff3c59891c282f5c119a8e9c2cb
santhosh-sri <santhosh.k@goeffortless.ai>
2026-04-03 13:51:47 +0530

    google site verificayion meta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Head, Html, Main, NextScript } from "next/document";
 
-
 export default function Document() {
   return (
-    <Html lang="en" >
-      <Head />
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="ixwYKeWv5u8KQUQ06uHWMyOOw_6DPszLPW_YTMezo4I"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
